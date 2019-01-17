795d83f67b7cf904d71700cfeabeeca8109cadf8
Peter Maksymowsky <peter.maksymowsky@mail.utoronto.ca>
2019-01-17 16:25:08 -0500

    Initial Milestone 1 commit by /nfs/ug/cad/cad2/ece297s/.public_v185/bin/.scripts/ece297init for maksymo4

---
+++ libstreetmap/src/m1.cpp
@@ -0,0 +1,43 @@
+/* 
+ * Copyright 2018 University of Toronto
+ *
+ * Permission is hereby granted, to use this software and associated 
+ * documentation files (the "Software") in course work at the University 
+ * of Toronto, or for personal use. Other uses are prohibited, in 
+ * particular the distribution of the Software either publicly or to third 
+ * parties.
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "m1.h"
+#include "StreetsDatabaseAPI.h"
+
+bool load_map(std::string /*map_path*/) {
+    bool load_successful = false; //Indicates whether the map has loaded 
+                                  //successfully
+
+    //
+    //Load your map related data structures here
+    //
+
+    
+
+    load_successful = true; //Make sure this is updated to reflect whether
+                            //loading the map succeeded or failed
+
+    return load_successful;
+}
+
+void close_map() {
+    //Clean-up your map related data structures here
+    
+}
