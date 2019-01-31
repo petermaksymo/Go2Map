8900ebf726e144f617f27280b4b6fc76674c268f
Hanke Liu <hankle.liu@mail.utoronto.ca>
2019-01-31 10:27:44 -0500

    Comments are added

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -136,27 +136,36 @@ void removeDuplicates(std::vector<Type>& vec) {
     vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
 }
 
+//Pre-compute length of street 
 double street_segment_length_helper(unsigned street_segment_id) {
     struct InfoStreetSegment street_segment = getInfoStreetSegment(street_segment_id);
     double distance = 0.0;
     int numOfCurves = street_segment.curvePointCount;
-    IntersectionIndex point1 = street_segment.from;
-    IntersectionIndex point2 = street_segment.to;
+    // Get
+    LatLon point1 = getIntersectionPosition(street_segment.from);
+    LatLon point2 = getIntersectionPosition(street_segment.to);
+    // In the case that street is straight 
     if (numOfCurves == 0) {
-        distance = find_distance_between_two_points(getIntersectionPosition(point1), getIntersectionPosition(point2));
-    } else if (numOfCurves == 1){
-        distance = find_distance_between_two_points(getIntersectionPosition(point1),getStreetSegmentCurvePoint(0, street_segment_id))
-        + find_distance_between_two_points(getStreetSegmentCurvePoint(0, street_segment_id), getIntersectionPosition(point2));          
+        distance = find_distance_between_two_points(point1, point2);
     } 
+    // In the case that street has 1 curve point
+    else if (numOfCurves == 1) {
+        // Calculate distance from two end points to the curve point
+        distance = find_distance_between_two_points(point1,getStreetSegmentCurvePoint(0, street_segment_id))
+        + find_distance_between_two_points(getStreetSegmentCurvePoint(0, street_segment_id), point2);          
+    } 
+    // In the case that street has more than 1 curve point
     else if (numOfCurves > 1) {
+        // Calculate distance between curve points
         for (int i = 0; i < numOfCurves - 1; i++) {
             distance = distance + 
             find_distance_between_two_points(getStreetSegmentCurvePoint(i, street_segment_id),
             getStreetSegmentCurvePoint(i+1, street_segment_id));
         }
+        // Adding distance of two end points to their adjacent curve points
         distance = distance + 
-        find_distance_between_two_points(getIntersectionPosition(point1),getStreetSegmentCurvePoint(0, street_segment_id))
-        + find_distance_between_two_points(getStreetSegmentCurvePoint(numOfCurves - 1, street_segment_id), getIntersectionPosition(point2));
+        find_distance_between_two_points(point1,getStreetSegmentCurvePoint(0, street_segment_id))
+        + find_distance_between_two_points(getStreetSegmentCurvePoint(numOfCurves - 1, street_segment_id), point2);
     }
     return distance;
 }
