8574ff4cda3c73e4754f5986f0dad5f00bd3134c
Peter Maksymowsky <peter.maksymowsky@mail.utoronto.ca>
2019-02-05 12:34:23 -0500

    Extracted helper functions to seperate file

---
+++ libstreetmap/src/helper_functions.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   helper_functions.h
+ *
+ */
+
+#pragma once //protects against multiple inclusions of this header file
+
+#include <vector>
+
+//sorts and removes all duplicates in a vectortemplate<typename Type>
+void removeDuplicates(std::vector<unsigned>& vec);
+
+//Pre-compute length of street 
+double street_segment_length_helper(unsigned street_segment_id);
+
+
--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -24,6 +24,7 @@
 #include <algorithm>
 #include <map>
 #include <boost/algorithm/string.hpp>
+#include "helper_functions.h"
 
 ////////////////////////////////////////////////
 ///// Structures for the MAP database  /////////
@@ -53,9 +54,6 @@ struct InfoStreetSegmentsLocal {
 /// END of Structures for the MAP database  ////
 ////////////////////////////////////////////////
 
-template<typename Type>
-void removeDuplicates(std::vector<Type>& vec);
-double street_segment_length_helper(unsigned street_segment_id);
 
 MapInfo MAP;
 InfoStreetSegmentsLocal LocalStreetSegments;
@@ -125,53 +123,6 @@ void close_map() {
     
 }
 
-////////////////////////////////////////////////
-//// Helper functions to be used for m1 ////////
-////////////////////////////////////////////////
-
-//sorts and removes all duplicates in a vector
-template<typename Type>
-void removeDuplicates(std::vector<Type>& vec) {
-    std::sort(vec.begin(), vec.end());
-    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
-}
-
-//Pre-compute length of street 
-double street_segment_length_helper(unsigned street_segment_id) {
-    struct InfoStreetSegment street_segment = getInfoStreetSegment(street_segment_id);
-    double distance = 0.0;
-    int numOfCurves = street_segment.curvePointCount;
-    // Get
-    LatLon point1 = getIntersectionPosition(street_segment.from);
-    LatLon point2 = getIntersectionPosition(street_segment.to);
-    // In the case that street is straight 
-    if (numOfCurves == 0) {
-        distance = find_distance_between_two_points(point1, point2);
-    } 
-    // In the case that street has 1 curve point
-    else if (numOfCurves == 1) {
-        // Calculate distance from two end points to the curve point
-        distance = find_distance_between_two_points(point1,getStreetSegmentCurvePoint(0, street_segment_id))
-        + find_distance_between_two_points(getStreetSegmentCurvePoint(0, street_segment_id), point2);          
-    } 
-    // In the case that street has more than 1 curve point
-    else if (numOfCurves > 1) {
-        // Calculate distance between curve points
-        for (int i = 0; i < numOfCurves - 1; i++) {
-            distance = distance + 
-            find_distance_between_two_points(getStreetSegmentCurvePoint(i, street_segment_id),
-            getStreetSegmentCurvePoint(i+1, street_segment_id));
-        }
-        // Adding distance of two end points to their adjacent curve points
-        distance = distance + 
-        find_distance_between_two_points(point1,getStreetSegmentCurvePoint(0, street_segment_id))
-        + find_distance_between_two_points(getStreetSegmentCurvePoint(numOfCurves - 1, street_segment_id), point2);
-    }
-    return distance;
-}
-////////////////////////////////////////////////
-////// End of helper functions for m1  /////////
-////////////////////////////////////////////////
 
 //was pre-computed in load_map for speed
 std::vector<unsigned> find_intersection_street_segments(unsigned intersection_id) {    
