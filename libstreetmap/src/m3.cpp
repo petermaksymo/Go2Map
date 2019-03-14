948670db6334743afd89891eb50c55e2d63f1195
Hanke Liu <hankle.liu@mail.utoronto.ca>
2019-03-14 01:06:24 -0400

    Modify find_turn_type but still fail a lot of test cases

--- libstreetmap/src/m3.cpp
+++ libstreetmap/src/m3.cpp
@@ -9,41 +9,72 @@
 #include <StreetsDatabaseAPI.h>
 #include <math.h>
 #include "helper_functions.h"
+#include <cmath>
 
 TurnType find_turn_type(unsigned street_segment1, unsigned street_segment2) {
     InfoStreetSegment segment1 = getInfoStreetSegment(street_segment1);
     InfoStreetSegment segment2 = getInfoStreetSegment(street_segment2);
     
-    double x1 = x_from_lon(getIntersectionPosition(segment1.from).lon());
-    double y1 = y_from_lat(getIntersectionPosition(segment1.from).lat());
-    double x2 = x_from_lon(getIntersectionPosition(segment1.to).lon());
-    double y2 = y_from_lat(getIntersectionPosition(segment1.to).lat());   
-    double slope_segment1 = (atan( (y2-y1)/(x2-x1) ) )/ DEG_TO_RAD;
+    double street1_x, street1_y, street2_x, street2_y;
+    int intersection_id = 0;
     
-    double x3 = x_from_lon(getIntersectionPosition(segment2.from).lon());
-    double y3 = y_from_lat(getIntersectionPosition(segment2.from).lat());
-    double x4 = x_from_lon(getIntersectionPosition(segment2.to).lon());
-    double y4 = y_from_lat(getIntersectionPosition(segment2.to).lat());   
-    double slope_segment2 = (atan( (y4-y3)/(x4-x3))) / DEG_TO_RAD;
+    // Check the intersectionIndex
+    if (segment1.to == segment2.to) intersection_id = segment1.to; 
+    else if (segment1.to == segment2.from) intersection_id = segment1.to; 
+    else if (segment1.from == segment2.from) intersection_id = segment1.from; 
+    else if (segment1.from == segment2.to) intersection_id = segment1.from;  
+    else return TurnType::NONE;
     
     // Return NONE if streets have same id
-    if (street_segment1 == street_segment2) return TurnType::NONE;
+    if (segment1.streetID == segment2.streetID) return TurnType::STRAIGHT;
     
-    // Return NONE if streets don't intersect
-    if (segment1.to != segment2.to|| 
-        segment1.to != segment2.from||
-        segment1.from != segment2.from||
-        segment1.from != segment2.to  
-            ) return TurnType::NONE;
+    // Calculate the slope of first street segment
+    // If the street has no curvepoint, then the other end is the end point
+    if (segment1.curvePointCount == 0) {
+        street1_x = x_from_lon(getIntersectionPosition(segment1.to).lon());
+        street1_y = y_from_lat(getIntersectionPosition(segment1.to).lat());
+    }
+    // If street1 has 'from' end as intersection, then first curvepoint is the end point for calculation
+    else if (segment1.from == intersection_id) { 
+        street1_x = x_from_lon(getStreetSegmentCurvePoint(0, street_segment1).lon());
+        street1_y = y_from_lat(getStreetSegmentCurvePoint(0, street_segment1).lat());
+    } 
+    // If street1 has 'to' end as intersection, then last curvepoint is the end point for calculation
+    else {
+        street1_x = x_from_lon(getStreetSegmentCurvePoint(segment1.curvePointCount - 1, street_segment1).lon());
+        street1_y = y_from_lat(getStreetSegmentCurvePoint(segment1.curvePointCount - 1, street_segment1).lat());
+    }
+    
+    // Calculate the slope of second street segment
+    if (segment2.curvePointCount == 0) {
+        street2_x = x_from_lon(getIntersectionPosition(segment2.to).lon());
+        street2_y = y_from_lat(getIntersectionPosition(segment2.to).lat());
+    } else if (segment1.from == intersection_id) {
+        street2_x = x_from_lon(getStreetSegmentCurvePoint(0, street_segment2).lon());
+        street2_y = y_from_lat(getStreetSegmentCurvePoint(0, street_segment2).lat());
+    } else {
+        street2_x = x_from_lon(getStreetSegmentCurvePoint(segment1.curvePointCount - 1, street_segment2).lon());
+        street2_y = y_from_lat(getStreetSegmentCurvePoint(segment1.curvePointCount - 1, street_segment2).lat());
+    }
+    
+    // Fetch x,y position of the intersection
+    double intersection_x = x_from_lon(getIntersectionPosition(intersection_id).lon());
+    double intersection_y = y_from_lat(getIntersectionPosition(intersection_id).lat());
+    
+    // Calculate the slope of each street segment
+    double slope_segment1; 
+    double slope_segment2;
+    if (street1_x - intersection_x == 0) slope_segment1 = 90; 
+    else if (abs(street1_y - intersection_y == 0)) slope_segment1 = 0;
+    else slope_segment1 = (atan(abs(street1_y - intersection_y) / abs(street1_x - intersection_x)) ) / DEG_TO_RAD;
+    
+    if (street2_x - intersection_x == 0) slope_segment2 = 90; 
+    else if (abs(street2_y - intersection_y == 0)) slope_segment2 = 0;
+    else slope_segment2 = (atan(abs(street2_y - intersection_y) / abs(street2_x - intersection_x)) ) / DEG_TO_RAD;
     
     if (slope_segment1 >= slope_segment2) return TurnType::RIGHT;
     if (slope_segment1 < slope_segment2) return TurnType::LEFT;
-    
-    
-    
-    
-    
-    
+      
 }
 
 double compute_path_travel_time(const std::vector<unsigned>& path, 
